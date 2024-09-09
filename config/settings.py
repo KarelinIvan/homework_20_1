c3939b43f61fb7af26736aa64363fa4895603db2
KarelinIvan <ivan.karelin.1993@mail.ru>
2024-09-09 18:15:44 +0300

    1-й коммит

---
+++ config/settings.py
@@ -0,0 +1,126 @@
+"""
+Настройки Django для проекта конфигурации.
+
+Создано 'django-admin startproject' с использованием Django 5.1.1.
+
+Для получения дополнительной информации об этом файле см.
+https://docs.djangoproject.com/en/5.1/topics/settings/
+
+Полный список настроек и их значений см.
+https://docs.djangoproject.com/en/5.1/ref/settings/
+"""
+
+from pathlib import Path
+
+# Создайте пути внутри проекта следующим образом: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Настройки быстрого старта разработки — не подходят для производства
+# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
+
+# ПРЕДУПРЕЖДЕНИЕ О БЕЗОПАСНОСТИ: храните в секрете секретный ключ, используемый в производстве!
+SECRET_KEY = 'django-insecure-hm*&0-545qd+hfr)pbg83#gcr$a2fo2wul@=u@s_(oc6d*1b4^'
+
+# ПРЕДУПРЕЖДЕНИЕ О БЕЗОПАСНОСТИ: не запускайте приложение с включенной отладкой в рабочей среде!
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+
+# Определение приложения
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'catalog',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'config.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'config.wsgi.application'
+
+
+# База данных
+# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
+# Проверка пароля
+# https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Интернационализация
+# https://docs.djangoproject.com/en/5.1/topics/i18n/
+
+LANGUAGE_CODE = 'ru-ru'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+# Статические файлы (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.1/howto/static-files/
+
+STATIC_URL = 'static/'
+
+STATICFILES_DIRS = (BASE_DIR / 'static',)
+
+# Тип поля первичного ключа по умолчанию
+# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
---
+++ config/urls.py
@@ -0,0 +1,24 @@
+"""
+Конфигурация URL для проекта конфигурации.
+
+Список `urlpatterns` направляет URL к представлениям. Для получения дополнительной информации см.:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+
+Примеры:
+Виды функций
+1. Добавьте импорт: from my_app import views
+2. Добавьте URL в urlpatterns: path('', views.home, name='home')
+Виды на основе классов
+1. Добавьте импорт: from other_app.views import Home
+2. Добавьте URL в urlpatterns: path('', Home.as_view(), name='home')
+Включение другого URLconf
+1. Импортируйте функцию include(): from django.urls import include, path
+2. Добавьте URL в urlpatterns: path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('catalog.urls', namespace='catalog'))
+]
