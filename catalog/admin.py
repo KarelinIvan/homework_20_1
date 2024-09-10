ab0ef9a7af5a04715cdcd7586fa2cda9c95398a1
KarelinIvan <ivan.karelin.1993@mail.ru>
2024-09-10 20:16:54 +0300

    12-й коммит, админка

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from catalog.models import Product, Category
+
+
 # Зарегистрируйте свои модели здесь.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "Purchase_price", "Category")
+    list_filter = ("Category",)
+    search_fields = ("name",)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name",)
