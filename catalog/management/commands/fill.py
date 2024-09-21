46cc5568e9b52d04ff1804c83c2ca2c386e5a29f
i.karelin <ivan,karelin.1993@mail.ru>
2024-09-21 07:53:53 +0300

    37-й коммит, правка

--- catalog/management/commands/fill.py
+++ catalog/management/commands/fill.py
@@ -12,17 +12,17 @@ class Command(BaseCommand):
     def json_read_categories():
         # Здесь мы получаем данные из фикстурв с категориями
         with open(file_json, 'r', encoding='utf-8') as file:
-            categories_data = file.read()
+            categories = file.read()
 
-        return json.loads(categories_data)
+        return json.loads(categories)
 
     @staticmethod
     def json_read_products():
         # Здесь мы получаем данные из фикстурв с продуктами
         with open(file_json, 'r', encoding='utf-8') as file:
-            products_data = file.read()
+            products = file.read()
 
-        return json.loads(products_data)
+        return json.loads(products)
 
     def handle(self, *args, **options):
         # Удалите все продукты
@@ -36,28 +36,31 @@ class Command(BaseCommand):
 
         # Обходим все значения категорий из фиктсуры для получения информации об одном объекте
         for category in Command.json_read_categories():
-            category_for_create.append(
-                Category(id=category['pk'],
-                         name=category["fields"]["name"],
-                         description=category["fields"]["description"])
-            )
+            if category['model'] == 'catalog.category':
+                category_for_create.append(
+                    Category(id=category['pk'],
+                            name=category["fields"]["name"],
+                            description=category["fields"]["description"],
+                             )
+                )
 
         # Создаем объекты в базе с помощью метода bulk_create()
         Category.objects.bulk_create(category_for_create)
 
         # Обходим все значения продуктов из фиктсуры для получения информации об одном объекте
         for product in Command.json_read_products():
-            product_for_create.append(
-                Product(pk=product["pk"],
-                        name=product["fields"]["name"],
-                        description=product["fields"]["description"],
-                        purchase_price=product["fields"]["purchase_price"],
-                        image=product["fields"]["image"],
-                        category=Category.objects.get(pk=product["fields"]["category"]),
-                        created_at=product["fields"]["created_at"],
-                        updated_at=product["fields"]["updated_at"],
-                        )
-            )
+            if product['model'] == 'catalog.product':
+                product_for_create.append(
+                    Product(pk=product["pk"],
+                            name=product["fields"]["name"],
+                            description=product["fields"]["description"],
+                            purchase_price=product["fields"]["purchase_price"],
+                            image=product["fields"]["image"],
+                            category=Category.objects.get(pk=product["fields"]["category"]),
+                            created_at=product["fields"]["created_at"],
+                            updated_at=product["fields"]["updated_at"],
+                            )
+                )
 
         # Создаем объекты в базе с помощью метода bulk_create()
         Product.objects.bulk_create(product_for_create)
