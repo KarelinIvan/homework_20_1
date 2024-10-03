3d9c2f1e764f7bf14ad42c993427807f4f65995e
KarelinIvan <ivan.karelin.1993@mail.ru>
2024-10-03 15:42:48 +0300

    фича, 4-й коммит

--- blog/utils.py
+++ blog/utils.py
@@ -1,5 +1,3 @@
 def upload_to(instance, filename):
-    """
-    Generate a file upload path based on the model type.
-    """
+
     return f"uploads/{instance.__class__.__name__.lower()}/{filename}"
