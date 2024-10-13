8b4073bad80378db0d1c54b29eec895e972a428f
KarelinIvan <ivan.karelin.1993@mail.ru>
2024-10-13 16:11:27 +0300

    126-й коммит, валидация

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, forms
 
 from catalog.models import Product
 
@@ -8,3 +8,26 @@ class ProductForm(ModelForm):
         model = Product
         fields = '__all__'
 
+    def clean_product_name(self):
+        cleaned_data = self.cleaned_data['name']
+
+        forbidden_words = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция',
+                           'радар']
+
+        for word in forbidden_words:
+            if word in cleaned_data:
+                raise forms.ValidationError('Данное название не подходит')
+
+        return cleaned_data
+
+    def clean_product_description(self):
+        cleaned_data = self.cleaned_data['description']
+
+        forbidden_words = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция',
+                           'радар']
+
+        for word in forbidden_words:
+            if word in cleaned_data:
+                raise forms.ValidationError('Данное описание не подходит')
+
+        return cleaned_data
--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,3 @@
-from django.forms import inlineformset_factory
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, TemplateView, CreateView, UpdateView, DeleteView
 
@@ -19,17 +18,42 @@ class ProductListView(ListView):
 class ProductDetailView(DetailView):
     model = Product
 
+
 class ProductCreateView(CreateView):
     model = Product
     form_class = ProductForm
     success_url = reverse_lazy("catalog:catalog_list")
+
+    def form_valid(self, form):
+        cleaned_data = form.cleaned_data['name'].lower()
+        forbidden_words = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция',
+                           'радар']
+
+        for word in forbidden_words:
+            if word in cleaned_data:
+                form.add_error('name', 'Данное название не подходит.')
+                return self.form_invalid(form)
+
+        return super().form_valid(form)
+
 
 class ProductUpdateView(UpdateView):
     model = Product
     form_class = ProductForm
     success_url = reverse_lazy("catalog:catalog_list")
 
+    def form_valid(self, form):
+        context_data = self.get_context_data()
+        formset = context_data['formset']
+        if form.is_valid() and formset.is_valid():
+            self.object = form.save()
+            formset.instance = self.object
+            formset.save()
+            return super().form_valid(form)
+        else:
+            return self.render_to_response(self.get_context_data(form=form, formset=formset))
+
+
 class ProductDeleteView(DeleteView):
     model = Product
     success_url = reverse_lazy('catalog:catalog_list')
-
