57b80b83a73a82881aa26cb74a4efcdefe6855ba
KarelinIvan <ivan.karelin.1993@mail.ru>
2024-10-14 16:52:44 +0300

    138-й коммит, регистрация нового пользователя

--- users/views.py
+++ users/views.py
@@ -1,7 +1,18 @@
 from django.contrib.auth import logout
 from django.shortcuts import redirect
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from users.forms import UserRegisterForm
+from users.models import User
 
 
 def logout_view(request):
     logout(request)
     return redirect('/')
+
+class RegisterView(CreateView):
+    model = User
+    form_class = UserRegisterForm
+    template_name = 'users/register.html'
+    success_url = reverse_lazy('users:login')
