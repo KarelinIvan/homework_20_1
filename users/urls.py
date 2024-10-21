0be4db9ae3d1731817ddd126ac6175041b76af74
KarelinIvan <ivan.karelin.1993@mail.ru>
2024-10-21 19:34:09 +0300

    152-й коммит, правка

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,8 @@ from django.contrib.auth.views import LoginView
 from django.urls import path
 
 from users.apps import UsersConfig
-from users.views import logout_view, RegisterView, ProfileView, email_verification, UserPasswordResetView
+from users.views import logout_view, RegisterView, ProfileView, email_verification, UserPasswordResetView, \
+    UserInValidEmail
 
 app_name = UsersConfig.name
 
@@ -12,5 +13,6 @@ urlpatterns = [
     path('register/', RegisterView.as_view(template_name='users/register.html'), name='register'),
     path('profile/', ProfileView.as_view(), name='profile'),
     path('email-confirm/<str:token>/', email_verification, name='email-confirm'),
-    path('reset-password/', UserPasswordResetView.as_view(), name='reset_password')
+    path('reset-password/', UserPasswordResetView.as_view(), name='reset_password'),
+    path("invalid-email/", UserInValidEmail.as_view(), name="invalid_email"),
 ]
