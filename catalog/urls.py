314957b9e181153eee2cccfc222bc52b88dae2a7
KarelinIvan <ivan.karelin.1993@mail.ru>
2024-11-04 14:06:13 +0300

    169-й коммит, кеширование контроллера ProductDetailView

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from catalog.apps import CatalogConfig
 from catalog.views import ProductListView, ProductDetailView, ContactsPageView, ProductCreateView, ProductUpdateView, \
@@ -9,7 +10,7 @@ app_name = CatalogConfig.name
 urlpatterns = [
     path("", ProductListView.as_view(), name="catalog_list"),
     path("contacts/", ContactsPageView.as_view(), name="contacts"),
-    path("catalog/<int:pk>/", ProductDetailView.as_view(), name="catalog_detail"),
+    path("catalog/<int:pk>/", cache_page(60)(ProductDetailView.as_view()), name="catalog_detail"),
     path('create/', ProductCreateView.as_view(), name="create_product"),
     path('update/<int:pk>/', ProductUpdateView.as_view(), name="product_update"),
     path('delete/<int:pk>/', ProductDeleteView.as_view(), name="product_delete"),
